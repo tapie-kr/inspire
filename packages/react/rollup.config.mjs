a43544f9fb6537c78044a3da2e2bce96dea94100
cottons-kr <taeyeong.kim@algorix.io>
2024-12-19 06:55:13 +0900

    🚧 add initial configuration and styles for React component library

---
+++ packages/react/rollup.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'rollup'
+import scss from 'rollup-plugin-scss'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+
+import { readFileSync } from 'fs'
+
+const packageJson = JSON.parse(
+  readFileSync(new URL('./package.json', import.meta.url)).toString()
+)
+
+const config = defineConfig([
+  {
+    input: 'src/styles/color/index.scss',
+    output: {
+      file: 'dist/bundle.js',
+    },
+    plugins: [
+      scss({
+        processor: () => postcss([autoprefixer()]),
+        // outputStyle: 'compressed',
+        fileName: 'styles.min.css',
+        includePaths: ['node_modules', 'src/styles'],
+        silenceDeprecations: ['legacy-js-api'],
+      }),
+    ],
+    onwarn(warning, warn) {
+      if (warning.code !== 'EMPTY_BUNDLE') {
+        warn(warning)
+      }
+    }
+  },
+])
+
+export default config
