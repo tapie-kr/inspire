79d8295c5d47aa924b086962ab75cb8710e0c083
cottons-kr <taeyeong.kim@algorix.io>
2025-01-10 10:03:35 +0900

    🔧 update Storybook configuration for path resolution; export Input and Label components in index

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -2,11 +2,9 @@ import type { StorybookConfig } from '@storybook/react-vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import svgr from 'vite-plugin-svgr'
 
-import { join, dirname, resolve } from 'path'
+import { resolve } from 'path'
 
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
+const currentPath = new URL('.', import.meta.url).pathname
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
@@ -15,14 +13,14 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
   viteFinal: (config) => {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        '@': resolve(__dirname, '../src'),
+        '@': resolve(currentPath, '../src'),
       }
     }
 
