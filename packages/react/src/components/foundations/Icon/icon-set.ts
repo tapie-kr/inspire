c9cebaab9d7e73a1ba76cd9760ef22473e0c24e7
cottons-kr <taeyeong.kim@algorix.io>
2025-01-11 07:36:38 +0900

    🍱 bump version to 0.0.48; add new SVG icons for various components

--- packages/react/src/components/foundations/Icon/icon-set.ts
+++ packages/react/src/components/foundations/Icon/icon-set.ts
@@ -1,37 +1,69 @@
 import ArrowBackIosGlyph from './assets/glyph/arrow_back_ios.svg'
 import ArrowDownwardGlyph from './assets/glyph/arrow_downward.svg'
 import ArrowForwardGlyph from './assets/glyph/arrow_forward.svg'
+import ArrowUpwardGlyph from './assets/glyph/arrow_upward.svg'
 import AsteriskGlyph from './assets/glyph/asterisk.svg'
+import AttachFileGlyph from './assets/glyph/attach_file.svg'
+import BadgeGlyph from './assets/glyph/badge.svg'
+import BlockGlyph from './assets/glyph/block.svg'
+import BookmarkGlyph from './assets/glyph/bookmark.svg'
 import BrushGlyph from './assets/glyph/brush.svg'
+import BugReportGlyph from './assets/glyph/bug_report.svg'
 import CheckGlyph from './assets/glyph/check.svg'
 import ChecklistGlyph from './assets/glyph/checklist.svg'
 import CloseGlyph from './assets/glyph/close.svg'
 import CodeGlyph from './assets/glyph/code.svg'
+import ComputerGlyph from './assets/glyph/computer.svg'
 import ContrastGlyph from './assets/glyph/contrast.svg'
 import CrownGlyph from './assets/glyph/crown.svg'
 import DarkModeGlyph from './assets/glyph/dark_mode.svg'
+import DataObjectGlyph from './assets/glyph/data_object.svg'
 import DefaultGlyph from './assets/glyph/default.svg'
 import DeleteGlyph from './assets/glyph/delete.svg'
+import DescriptionGlyph from './assets/glyph/description.svg'
 import DownloadGlyph from './assets/glyph/download.svg'
+import DraftGlyph from './assets/glyph/draft.svg'
+import ErrorGlyph from './assets/glyph/error.svg'
+import ExploreGlyph from './assets/glyph/explore.svg'
 import FaceGlyph from './assets/glyph/face.svg'
+import FingerprintGlyph from './assets/glyph/fingerprint.svg'
 import FlagGlyph from './assets/glyph/flag.svg'
+import FolderGlyph from './assets/glyph/folder.svg'
+import FolderOpenGlyph from './assets/glyph/folder_open.svg'
 import GroupsGlyph from './assets/glyph/groups.svg'
+import HelpGlyph from './assets/glyph/help.svg'
 import HomeGlyph from './assets/glyph/home.svg'
+import InfoGlyph from './assets/glyph/info.svg'
 import KeyboardArrowUpGlyph from './assets/glyph/keyboard_arrow_up.svg'
+import LabelGlyph from './assets/glyph/label.svg'
+import LanguageGlyph from './assets/glyph/language.svg'
+import LayersGlyph from './assets/glyph/layers.svg'
 import LightModeGlyph from './assets/glyph/light_mode.svg'
 import LinkGlyph from './assets/glyph/link.svg'
+import LockGlyph from './assets/glyph/lock.svg'
+import LockOpenGlyph from './assets/glyph/lock_open.svg'
+import LogoutGlyph from './assets/glyph/logout.svg'
+import MemoryGlyph from './assets/glyph/memory.svg'
 import MenuGlyph from './assets/glyph/menu.svg'
 import NorthEastGlyph from './assets/glyph/north_east.svg'
+import PaletteGlyph from './assets/glyph/palette.svg'
+import PhotoCameraGlyph from './assets/glyph/photo_camera.svg'
+import PublicGlyph from './assets/glyph/public.svg'
 import QrCodeScannerGlyph from './assets/glyph/qr_code_scanner.svg'
 import RefreshGlyph from './assets/glyph/refresh.svg'
+import ReplyGlyph from './assets/glyph/reply.svg'
 import RestartAltGlyph from './assets/glyph/restart_alt.svg'
 import SchoolGlyph from './assets/glyph/school.svg'
 import SearchGlyph from './assets/glyph/search.svg'
 import ShareGlyph from './assets/glyph/share.svg'
+import ShieldGlyph from './assets/glyph/shield.svg'
+import StarGlyph from './assets/glyph/star.svg'
+import SyncGlyph from './assets/glyph/sync.svg'
 import TrophyGlyph from './assets/glyph/trophy.svg'
 import UploadGlyph from './assets/glyph/upload.svg'
 import VerifiedGlyph from './assets/glyph/verified.svg'
 import VisibilityGlyph from './assets/glyph/visibility.svg'
+import WarningGlyph from './assets/glyph/warning.svg'
 
 import AndroidBrand from './assets/brand/android.svg'
 import AppStoreBrand from './assets/brand/app_store.svg'
@@ -77,74 +109,138 @@ export enum GlyphIcon {
   ARROW_BACK_IOS = 'arrow_back_ios',
   ARROW_DOWNWARD = 'arrow_downward',
   ARROW_FORWARD = 'arrow_forward',
+  ARROW_UPWARD = 'arrow_upward',
   ASTERISK = 'asterisk',
+  ATTACH_FILE = 'attach_file',
+  BADGE = 'badge',
+  BLOCK = 'block',
+  BOOKMARK = 'bookmark',
   BRUSH = 'brush',
+  BUG_REPORT = 'bug_report',
   CHECK = 'check',
   CHECKLIST = 'checklist',
   CLOSE = 'close',
   CODE = 'code',
+  COMPUTER = 'computer',
   CONTRAST = 'contrast',
   CROWN = 'crown',
   DARK_MODE = 'dark_mode',
+  DATA_OBJECT = 'data_object',
   DEFAULT = 'default',
   DELETE = 'delete',
+  DESCRIPTION = 'description',
   DOWNLOAD = 'download',
+  DRAFT = 'draft',
+  ERROR = 'error',
+  EXPLORE = 'explore',
   FACE = 'face',
+  FINGERPRINT = 'fingerprint',
   FLAG = 'flag',
+  FOLDER = 'folder',
+  FOLDER_OPEN = 'folder_open',
   GROUPS = 'groups',
+  HELP = 'help',
   HOME = 'home',
+  INFO = 'info',
   KEYBOARD_ARROW_UP = 'keyboard_arrow_up',
+  LABEL = 'label',
+  LANGUAGE = 'language',
+  LAYERS = 'layers',
   LIGHT_MODE = 'light_mode',
   LINK = 'link',
+  LOCK = 'lock',
+  LOCK_OPEN = 'lock_open',
+  LOGOUT = 'logout',
+  MEMORY = 'memory',
   MENU = 'menu',
   NORTH_EAST = 'north_east',
+  PALETTE = 'palette',
+  PHOTO_CAMERA = 'photo_camera',
+  PUBLIC = 'public',
   QR_CODE_SCANNER = 'qr_code_scanner',
   REFRESH = 'refresh',
+  REPLY = 'reply',
   RESTART_ALT = 'restart_alt',
   SCHOOL = 'school',
   SEARCH = 'search',
   SHARE = 'share',
+  SHIELD = 'shield',
+  STAR = 'star',
+  SYNC = 'sync',
   TROPHY = 'trophy',
   UPLOAD = 'upload',
   VERIFIED = 'verified',
-  VISIBILITY = 'visibility'
+  VISIBILITY = 'visibility',
+  WARNING = 'warning'
 }
 
 export const GlyphIconMap = {
   [GlyphIcon.ARROW_BACK_IOS]: ArrowBackIosGlyph,
   [GlyphIcon.ARROW_DOWNWARD]: ArrowDownwardGlyph,
   [GlyphIcon.ARROW_FORWARD]: ArrowForwardGlyph,
+  [GlyphIcon.ARROW_UPWARD]: ArrowUpwardGlyph,
   [GlyphIcon.ASTERISK]: AsteriskGlyph,
+  [GlyphIcon.ATTACH_FILE]: AttachFileGlyph,
+  [GlyphIcon.BADGE]: BadgeGlyph,
+  [GlyphIcon.BLOCK]: BlockGlyph,
+  [GlyphIcon.BOOKMARK]: BookmarkGlyph,
   [GlyphIcon.BRUSH]: BrushGlyph,
+  [GlyphIcon.BUG_REPORT]: BugReportGlyph,
   [GlyphIcon.CHECK]: CheckGlyph,
   [GlyphIcon.CHECKLIST]: ChecklistGlyph,
   [GlyphIcon.CLOSE]: CloseGlyph,
   [GlyphIcon.CODE]: CodeGlyph,
+  [GlyphIcon.COMPUTER]: ComputerGlyph,
   [GlyphIcon.CONTRAST]: ContrastGlyph,
   [GlyphIcon.CROWN]: CrownGlyph,
   [GlyphIcon.DARK_MODE]: DarkModeGlyph,
+  [GlyphIcon.DATA_OBJECT]: DataObjectGlyph,
   [GlyphIcon.DEFAULT]: DefaultGlyph,
   [GlyphIcon.DELETE]: DeleteGlyph,
+  [GlyphIcon.DESCRIPTION]: DescriptionGlyph,
   [GlyphIcon.DOWNLOAD]: DownloadGlyph,
+  [GlyphIcon.DRAFT]: DraftGlyph,
+  [GlyphIcon.ERROR]: ErrorGlyph,
+  [GlyphIcon.EXPLORE]: ExploreGlyph,
   [GlyphIcon.FACE]: FaceGlyph,
+  [GlyphIcon.FINGERPRINT]: FingerprintGlyph,
   [GlyphIcon.FLAG]: FlagGlyph,
+  [GlyphIcon.FOLDER]: FolderGlyph,
+  [GlyphIcon.FOLDER_OPEN]: FolderOpenGlyph,
   [GlyphIcon.GROUPS]: GroupsGlyph,
+  [GlyphIcon.HELP]: HelpGlyph,
   [GlyphIcon.HOME]: HomeGlyph,
+  [GlyphIcon.INFO]: InfoGlyph,
   [GlyphIcon.KEYBOARD_ARROW_UP]: KeyboardArrowUpGlyph,
+  [GlyphIcon.LABEL]: LabelGlyph,
+  [GlyphIcon.LANGUAGE]: LanguageGlyph,
+  [GlyphIcon.LAYERS]: LayersGlyph,
   [GlyphIcon.LIGHT_MODE]: LightModeGlyph,
   [GlyphIcon.LINK]: LinkGlyph,
+  [GlyphIcon.LOCK]: LockGlyph,
+  [GlyphIcon.LOCK_OPEN]: LockOpenGlyph,
+  [GlyphIcon.LOGOUT]: LogoutGlyph,
+  [GlyphIcon.MEMORY]: MemoryGlyph,
   [GlyphIcon.MENU]: MenuGlyph,
   [GlyphIcon.NORTH_EAST]: NorthEastGlyph,
+  [GlyphIcon.PALETTE]: PaletteGlyph,
+  [GlyphIcon.PHOTO_CAMERA]: PhotoCameraGlyph,
+  [GlyphIcon.PUBLIC]: PublicGlyph,
   [GlyphIcon.QR_CODE_SCANNER]: QrCodeScannerGlyph,
   [GlyphIcon.REFRESH]: RefreshGlyph,
+  [GlyphIcon.REPLY]: ReplyGlyph,
   [GlyphIcon.RESTART_ALT]: RestartAltGlyph,
   [GlyphIcon.SCHOOL]: SchoolGlyph,
   [GlyphIcon.SEARCH]: SearchGlyph,
   [GlyphIcon.SHARE]: ShareGlyph,
+  [GlyphIcon.SHIELD]: ShieldGlyph,
+  [GlyphIcon.STAR]: StarGlyph,
+  [GlyphIcon.SYNC]: SyncGlyph,
   [GlyphIcon.TROPHY]: TrophyGlyph,
   [GlyphIcon.UPLOAD]: UploadGlyph,
   [GlyphIcon.VERIFIED]: VerifiedGlyph,
-  [GlyphIcon.VISIBILITY]: VisibilityGlyph
+  [GlyphIcon.VISIBILITY]: VisibilityGlyph,
+  [GlyphIcon.WARNING]: WarningGlyph
 } as const
 
 export enum BrandIcon {
