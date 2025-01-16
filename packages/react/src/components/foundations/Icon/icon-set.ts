0f00b8fb3ace54b91db94b5c0078a88aa72abfe6
cottons-kr <taeyeong.kim@algorix.io>
2025-01-16 18:53:33 +0900

    🍱 update package version to 0.1.7; add new SVG icons for various functionalities

--- packages/react/src/components/foundations/Icon/icon-set.ts
+++ packages/react/src/components/foundations/Icon/icon-set.ts
@@ -37,6 +37,8 @@ import UnityBrand from './assets/brand/unity.svg';
 import VercelBrand from './assets/brand/vercel.svg';
 import VisibilityOffBrand from './assets/brand/visibility_off.svg';
 import YarnBerryBrand from './assets/brand/yarn_berry.svg';
+import AddGlyph from './assets/glyph/add.svg';
+import AddPhotoAlternateGlyph from './assets/glyph/add_photo_alternate.svg';
 import ArrowBackGlyph from './assets/glyph/arrow_back.svg';
 import ArrowBackIosGlyph from './assets/glyph/arrow_back_ios.svg';
 import ArrowDownwardGlyph from './assets/glyph/arrow_downward.svg';
@@ -46,66 +48,100 @@ import AsteriskGlyph from './assets/glyph/asterisk.svg';
 import AttachFileGlyph from './assets/glyph/attach_file.svg';
 import BadgeGlyph from './assets/glyph/badge.svg';
 import BlockGlyph from './assets/glyph/block.svg';
+import BookRibbonGlyph from './assets/glyph/book_ribbon.svg';
 import BookmarkGlyph from './assets/glyph/bookmark.svg';
 import BrushGlyph from './assets/glyph/brush.svg';
 import BugReportGlyph from './assets/glyph/bug_report.svg';
+import CalendarMonthGlyph from './assets/glyph/calendar_month.svg';
+import CalendarTodayGlyph from './assets/glyph/calendar_today.svg';
+import CategorySearchGlyph from './assets/glyph/category_search.svg';
 import CheckGlyph from './assets/glyph/check.svg';
 import ChecklistGlyph from './assets/glyph/checklist.svg';
+import ChevronLeftGlyph from './assets/glyph/chevron_left.svg';
+import ChevronRightGlyph from './assets/glyph/chevron_right.svg';
 import CloseGlyph from './assets/glyph/close.svg';
 import CodeGlyph from './assets/glyph/code.svg';
 import ComputerGlyph from './assets/glyph/computer.svg';
 import ContrastGlyph from './assets/glyph/contrast.svg';
+import CoronavirusGlyph from './assets/glyph/coronavirus.svg';
 import CrownGlyph from './assets/glyph/crown.svg';
 import DarkModeGlyph from './assets/glyph/dark_mode.svg';
 import DataObjectGlyph from './assets/glyph/data_object.svg';
+import DateRangeGlyph from './assets/glyph/date_range.svg';
 import DefaultGlyph from './assets/glyph/default.svg';
 import DeleteGlyph from './assets/glyph/delete.svg';
 import DescriptionGlyph from './assets/glyph/description.svg';
 import DownloadGlyph from './assets/glyph/download.svg';
 import DraftGlyph from './assets/glyph/draft.svg';
+import EditGlyph from './assets/glyph/edit.svg';
 import ErrorGlyph from './assets/glyph/error.svg';
+import EventGlyph from './assets/glyph/event.svg';
+import ExpandAllGlyph from './assets/glyph/expand_all.svg';
 import ExploreGlyph from './assets/glyph/explore.svg';
 import FaceGlyph from './assets/glyph/face.svg';
+import FemaleGlyph from './assets/glyph/female.svg';
+import FilterListGlyph from './assets/glyph/filter_list.svg';
 import FingerprintGlyph from './assets/glyph/fingerprint.svg';
 import FlagGlyph from './assets/glyph/flag.svg';
 import FolderGlyph from './assets/glyph/folder.svg';
 import FolderOpenGlyph from './assets/glyph/folder_open.svg';
+import ForumGlyph from './assets/glyph/forum.svg';
 import GroupsGlyph from './assets/glyph/groups.svg';
 import HelpGlyph from './assets/glyph/help.svg';
 import HomeGlyph from './assets/glyph/home.svg';
+import ImageGlyph from './assets/glyph/image.svg';
+import InboxGlyph from './assets/glyph/inbox.svg';
 import InfoGlyph from './assets/glyph/info.svg';
+import KeyboardArrowDownGlyph from './assets/glyph/keyboard_arrow_down.svg';
 import KeyboardArrowUpGlyph from './assets/glyph/keyboard_arrow_up.svg';
+import KeyboardArrowUpCopyGlyph from './assets/glyph/keyboard_arrow_up_copy.svg';
 import LabelGlyph from './assets/glyph/label.svg';
 import LanguageGlyph from './assets/glyph/language.svg';
 import LayersGlyph from './assets/glyph/layers.svg';
+import LeftPanelCloseGlyph from './assets/glyph/left_panel_close.svg';
+import LeftPanelOpenGlyph from './assets/glyph/left_panel_open.svg';
 import LightModeGlyph from './assets/glyph/light_mode.svg';
 import LinkGlyph from './assets/glyph/link.svg';
 import LockGlyph from './assets/glyph/lock.svg';
 import LockOpenGlyph from './assets/glyph/lock_open.svg';
+import LoginGlyph from './assets/glyph/login.svg';
 import LogoutGlyph from './assets/glyph/logout.svg';
+import MaleGlyph from './assets/glyph/male.svg';
 import MemoryGlyph from './assets/glyph/memory.svg';
 import MenuGlyph from './assets/glyph/menu.svg';
+import MovieGlyph from './assets/glyph/movie.svg';
 import NorthEastGlyph from './assets/glyph/north_east.svg';
+import NotificationsGlyph from './assets/glyph/notifications.svg';
 import PaletteGlyph from './assets/glyph/palette.svg';
+import PaletteCopyGlyph from './assets/glyph/palette_copy.svg';
+import PendingGlyph from './assets/glyph/pending.svg';
 import PhotoCameraGlyph from './assets/glyph/photo_camera.svg';
 import PublicGlyph from './assets/glyph/public.svg';
 import QrCodeScannerGlyph from './assets/glyph/qr_code_scanner.svg';
 import RefreshGlyph from './assets/glyph/refresh.svg';
 import ReplyGlyph from './assets/glyph/reply.svg';
 import RestartAltGlyph from './assets/glyph/restart_alt.svg';
+import ScheduleGlyph from './assets/glyph/schedule.svg';
 import SchoolGlyph from './assets/glyph/school.svg';
 import SearchGlyph from './assets/glyph/search.svg';
+import SendGlyph from './assets/glyph/send.svg';
 import ShareGlyph from './assets/glyph/share.svg';
 import ShieldGlyph from './assets/glyph/shield.svg';
+import SortGlyph from './assets/glyph/sort.svg';
 import StarGlyph from './assets/glyph/star.svg';
 import SyncGlyph from './assets/glyph/sync.svg';
+import TerminalGlyph from './assets/glyph/terminal.svg';
+import TodayGlyph from './assets/glyph/today.svg';
 import TrophyGlyph from './assets/glyph/trophy.svg';
+import TuneGlyph from './assets/glyph/tune.svg';
 import UploadGlyph from './assets/glyph/upload.svg';
 import VerifiedGlyph from './assets/glyph/verified.svg';
 import VisibilityGlyph from './assets/glyph/visibility.svg';
 import WarningGlyph from './assets/glyph/warning.svg';
 
 export enum GlyphIcon {
+  ADD = 'add',
+  ADD_PHOTO_ALTERNATE = 'add_photo_alternate',
   ARROW_BACK = 'arrow_back',
   ARROW_BACK_IOS = 'arrow_back_ios',
   ARROW_DOWNWARD = 'arrow_downward',
@@ -115,60 +151,92 @@ export enum GlyphIcon {
   ATTACH_FILE = 'attach_file',
   BADGE = 'badge',
   BLOCK = 'block',
+  BOOK_RIBBON = 'book_ribbon',
   BOOKMARK = 'bookmark',
   BRUSH = 'brush',
   BUG_REPORT = 'bug_report',
+  CALENDAR_MONTH = 'calendar_month',
+  CALENDAR_TODAY = 'calendar_today',
+  CATEGORY_SEARCH = 'category_search',
   CHECK = 'check',
   CHECKLIST = 'checklist',
+  CHEVRON_LEFT = 'chevron_left',
+  CHEVRON_RIGHT = 'chevron_right',
   CLOSE = 'close',
   CODE = 'code',
   COMPUTER = 'computer',
   CONTRAST = 'contrast',
+  CORONAVIRUS = 'coronavirus',
   CROWN = 'crown',
   DARK_MODE = 'dark_mode',
   DATA_OBJECT = 'data_object',
+  DATE_RANGE = 'date_range',
   DEFAULT = 'default',
   DELETE = 'delete',
   DESCRIPTION = 'description',
   DOWNLOAD = 'download',
   DRAFT = 'draft',
+  EDIT = 'edit',
   ERROR = 'error',
+  EVENT = 'event',
+  EXPAND_ALL = 'expand_all',
   EXPLORE = 'explore',
   FACE = 'face',
+  FEMALE = 'female',
+  FILTER_LIST = 'filter_list',
   FINGERPRINT = 'fingerprint',
   FLAG = 'flag',
   FOLDER = 'folder',
   FOLDER_OPEN = 'folder_open',
+  FORUM = 'forum',
   GROUPS = 'groups',
   HELP = 'help',
   HOME = 'home',
+  IMAGE = 'image',
+  INBOX = 'inbox',
   INFO = 'info',
+  KEYBOARD_ARROW_DOWN = 'keyboard_arrow_down',
   KEYBOARD_ARROW_UP = 'keyboard_arrow_up',
+  KEYBOARD_ARROW_UP_COPY = 'keyboard_arrow_up_copy',
   LABEL = 'label',
   LANGUAGE = 'language',
   LAYERS = 'layers',
+  LEFT_PANEL_CLOSE = 'left_panel_close',
+  LEFT_PANEL_OPEN = 'left_panel_open',
   LIGHT_MODE = 'light_mode',
   LINK = 'link',
   LOCK = 'lock',
   LOCK_OPEN = 'lock_open',
+  LOGIN = 'login',
   LOGOUT = 'logout',
+  MALE = 'male',
   MEMORY = 'memory',
   MENU = 'menu',
+  MOVIE = 'movie',
   NORTH_EAST = 'north_east',
+  NOTIFICATIONS = 'notifications',
   PALETTE = 'palette',
+  PALETTE_COPY = 'palette_copy',
+  PENDING = 'pending',
   PHOTO_CAMERA = 'photo_camera',
   PUBLIC = 'public',
   QR_CODE_SCANNER = 'qr_code_scanner',
   REFRESH = 'refresh',
   REPLY = 'reply',
   RESTART_ALT = 'restart_alt',
+  SCHEDULE = 'schedule',
   SCHOOL = 'school',
   SEARCH = 'search',
+  SEND = 'send',
   SHARE = 'share',
   SHIELD = 'shield',
+  SORT = 'sort',
   STAR = 'star',
   SYNC = 'sync',
+  TERMINAL = 'terminal',
+  TODAY = 'today',
   TROPHY = 'trophy',
+  TUNE = 'tune',
   UPLOAD = 'upload',
   VERIFIED = 'verified',
   VISIBILITY = 'visibility',
@@ -176,6 +244,8 @@ export enum GlyphIcon {
 }
 
 export const GlyphIconMap = {
+  [GlyphIcon.ADD]: AddGlyph,
+  [GlyphIcon.ADD_PHOTO_ALTERNATE]: AddPhotoAlternateGlyph,
   [GlyphIcon.ARROW_BACK]: ArrowBackGlyph,
   [GlyphIcon.ARROW_BACK_IOS]: ArrowBackIosGlyph,
   [GlyphIcon.ARROW_DOWNWARD]: ArrowDownwardGlyph,
@@ -185,60 +255,92 @@ export const GlyphIconMap = {
   [GlyphIcon.ATTACH_FILE]: AttachFileGlyph,
   [GlyphIcon.BADGE]: BadgeGlyph,
   [GlyphIcon.BLOCK]: BlockGlyph,
+  [GlyphIcon.BOOK_RIBBON]: BookRibbonGlyph,
   [GlyphIcon.BOOKMARK]: BookmarkGlyph,
   [GlyphIcon.BRUSH]: BrushGlyph,
   [GlyphIcon.BUG_REPORT]: BugReportGlyph,
+  [GlyphIcon.CALENDAR_MONTH]: CalendarMonthGlyph,
+  [GlyphIcon.CALENDAR_TODAY]: CalendarTodayGlyph,
+  [GlyphIcon.CATEGORY_SEARCH]: CategorySearchGlyph,
   [GlyphIcon.CHECK]: CheckGlyph,
   [GlyphIcon.CHECKLIST]: ChecklistGlyph,
+  [GlyphIcon.CHEVRON_LEFT]: ChevronLeftGlyph,
+  [GlyphIcon.CHEVRON_RIGHT]: ChevronRightGlyph,
   [GlyphIcon.CLOSE]: CloseGlyph,
   [GlyphIcon.CODE]: CodeGlyph,
   [GlyphIcon.COMPUTER]: ComputerGlyph,
   [GlyphIcon.CONTRAST]: ContrastGlyph,
+  [GlyphIcon.CORONAVIRUS]: CoronavirusGlyph,
   [GlyphIcon.CROWN]: CrownGlyph,
   [GlyphIcon.DARK_MODE]: DarkModeGlyph,
   [GlyphIcon.DATA_OBJECT]: DataObjectGlyph,
+  [GlyphIcon.DATE_RANGE]: DateRangeGlyph,
   [GlyphIcon.DEFAULT]: DefaultGlyph,
   [GlyphIcon.DELETE]: DeleteGlyph,
   [GlyphIcon.DESCRIPTION]: DescriptionGlyph,
   [GlyphIcon.DOWNLOAD]: DownloadGlyph,
   [GlyphIcon.DRAFT]: DraftGlyph,
+  [GlyphIcon.EDIT]: EditGlyph,
   [GlyphIcon.ERROR]: ErrorGlyph,
+  [GlyphIcon.EVENT]: EventGlyph,
+  [GlyphIcon.EXPAND_ALL]: ExpandAllGlyph,
   [GlyphIcon.EXPLORE]: ExploreGlyph,
   [GlyphIcon.FACE]: FaceGlyph,
+  [GlyphIcon.FEMALE]: FemaleGlyph,
+  [GlyphIcon.FILTER_LIST]: FilterListGlyph,
   [GlyphIcon.FINGERPRINT]: FingerprintGlyph,
   [GlyphIcon.FLAG]: FlagGlyph,
   [GlyphIcon.FOLDER]: FolderGlyph,
   [GlyphIcon.FOLDER_OPEN]: FolderOpenGlyph,
+  [GlyphIcon.FORUM]: ForumGlyph,
   [GlyphIcon.GROUPS]: GroupsGlyph,
   [GlyphIcon.HELP]: HelpGlyph,
   [GlyphIcon.HOME]: HomeGlyph,
+  [GlyphIcon.IMAGE]: ImageGlyph,
+  [GlyphIcon.INBOX]: InboxGlyph,
   [GlyphIcon.INFO]: InfoGlyph,
+  [GlyphIcon.KEYBOARD_ARROW_DOWN]: KeyboardArrowDownGlyph,
   [GlyphIcon.KEYBOARD_ARROW_UP]: KeyboardArrowUpGlyph,
+  [GlyphIcon.KEYBOARD_ARROW_UP_COPY]: KeyboardArrowUpCopyGlyph,
   [GlyphIcon.LABEL]: LabelGlyph,
   [GlyphIcon.LANGUAGE]: LanguageGlyph,
   [GlyphIcon.LAYERS]: LayersGlyph,
+  [GlyphIcon.LEFT_PANEL_CLOSE]: LeftPanelCloseGlyph,
+  [GlyphIcon.LEFT_PANEL_OPEN]: LeftPanelOpenGlyph,
   [GlyphIcon.LIGHT_MODE]: LightModeGlyph,
   [GlyphIcon.LINK]: LinkGlyph,
   [GlyphIcon.LOCK]: LockGlyph,
   [GlyphIcon.LOCK_OPEN]: LockOpenGlyph,
+  [GlyphIcon.LOGIN]: LoginGlyph,
   [GlyphIcon.LOGOUT]: LogoutGlyph,
+  [GlyphIcon.MALE]: MaleGlyph,
   [GlyphIcon.MEMORY]: MemoryGlyph,
   [GlyphIcon.MENU]: MenuGlyph,
+  [GlyphIcon.MOVIE]: MovieGlyph,
   [GlyphIcon.NORTH_EAST]: NorthEastGlyph,
+  [GlyphIcon.NOTIFICATIONS]: NotificationsGlyph,
   [GlyphIcon.PALETTE]: PaletteGlyph,
+  [GlyphIcon.PALETTE_COPY]: PaletteCopyGlyph,
+  [GlyphIcon.PENDING]: PendingGlyph,
   [GlyphIcon.PHOTO_CAMERA]: PhotoCameraGlyph,
   [GlyphIcon.PUBLIC]: PublicGlyph,
   [GlyphIcon.QR_CODE_SCANNER]: QrCodeScannerGlyph,
   [GlyphIcon.REFRESH]: RefreshGlyph,
   [GlyphIcon.REPLY]: ReplyGlyph,
   [GlyphIcon.RESTART_ALT]: RestartAltGlyph,
+  [GlyphIcon.SCHEDULE]: ScheduleGlyph,
   [GlyphIcon.SCHOOL]: SchoolGlyph,
   [GlyphIcon.SEARCH]: SearchGlyph,
+  [GlyphIcon.SEND]: SendGlyph,
   [GlyphIcon.SHARE]: ShareGlyph,
   [GlyphIcon.SHIELD]: ShieldGlyph,
+  [GlyphIcon.SORT]: SortGlyph,
   [GlyphIcon.STAR]: StarGlyph,
   [GlyphIcon.SYNC]: SyncGlyph,
+  [GlyphIcon.TERMINAL]: TerminalGlyph,
+  [GlyphIcon.TODAY]: TodayGlyph,
   [GlyphIcon.TROPHY]: TrophyGlyph,
+  [GlyphIcon.TUNE]: TuneGlyph,
   [GlyphIcon.UPLOAD]: UploadGlyph,
   [GlyphIcon.VERIFIED]: VerifiedGlyph,
   [GlyphIcon.VISIBILITY]: VisibilityGlyph,
