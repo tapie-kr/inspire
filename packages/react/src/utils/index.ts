f88f02edc08c6d682e5e06036c6c54ee0cf4b72a
cottons-kr <taeyeong.kim@algorix.io>
2025-01-20 03:38:09 +0900

    ⚰️ remove unused barrel files in react package

--- packages/react/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './string/capitalize-first-letter';
-export * from './style/color/opacity';
-export * from './style/layer';
-export * from './style/responsive';
-export * from './style/shorthand';
