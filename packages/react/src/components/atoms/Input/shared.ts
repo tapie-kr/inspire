f90c8aad1bb8d01faa5e5d9ad8766196f23d8df3
cottons-kr <taeyeong.kim@algorix.io>
2025-01-21 22:30:58 +0900

    🐛 update package version to 0.1.15; omit 'size' property from HTML input and textarea props

--- packages/react/src/components/atoms/Input/shared.ts
+++ packages/react/src/components/atoms/Input/shared.ts
@@ -1,7 +1,7 @@
 import { type InputHTMLAttributes } from 'react';
 
-export type HTMLInputProps = InputHTMLAttributes<HTMLInputElement>;
-export type HTMLTextAreaProps = InputHTMLAttributes<HTMLTextAreaElement>;
+export type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'size'>;
+export type HTMLTextAreaProps = Omit<InputHTMLAttributes<HTMLTextAreaElement>, 'size'>;
 
 export enum InputSize {
   LARGE = 'large',
