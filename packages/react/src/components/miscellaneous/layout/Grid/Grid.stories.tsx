cc44127d9062b2cd5f1e4f8ec5c498d7967bb89c
cottons-kr <taeyeong.kim@algorix.io>
2025-01-26 02:56:54 +0900

    ✨ add Grid component with customizable layout options and stories; update package version to 0.1.30

---
+++ packages/react/src/components/miscellaneous/layout/Grid/Grid.stories.tsx
@@ -0,0 +1,69 @@
+import { colorVars } from '@/lib/style/contract/color.css';
+import { radiusVars } from '@/lib/style/contract/component.css';
+
+import { type Meta, type StoryObj } from '@storybook/react';
+import { Box } from '../Box';
+import { Grid } from '.';
+
+const meta: Meta = {
+  title: 'Miscellaneous/Layout/Grid',
+  component: Grid,
+  argTypes: {
+    gap: {
+      control: { type: 'number' },
+    },
+    rowGap: {
+      control: { type: 'number' },
+    },
+    columnGap: {
+      control: { type: 'number' },
+    },
+    columnCount: {
+      control: { type: 'number' },
+    },
+    fullWidth: {
+      control: { type: 'boolean' },
+    },
+    fullHeight: {
+      control: { type: 'boolean' },
+    },
+  },
+  args: {
+    gap: 10,
+    rowGap: 10,
+    columnGap: 10,
+    columnCount: 3,
+  },
+};
+
+type GridStory = StoryObj<typeof Grid>;
+
+export const Default: GridStory = {
+  render: props => (
+    <Box style={{ width: 600 }}>
+      <Grid {...props}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </Grid>
+    </Box>
+  ),
+};
+
+export default meta;
+
+function Item() {
+  return (
+    <Box
+      fullWidth
+      style={{
+        height: 150,
+        background: colorVars.surface.raised,
+        borderRadius: radiusVars.default,
+      }}
+    />
+  );
+}
---
+++ packages/react/src/components/miscellaneous/layout/Grid/index.tsx
@@ -0,0 +1,34 @@
+import { grid } from './styles.css';
+
+import cn from 'classnames';
+import { type DefaultProps } from '@/types/props';
+import { Box } from '../Box';
+
+type GridProps = DefaultProps & {
+  gap?: string | number;
+  rowGap?: string | number;
+  columnGap?: string | number;
+  columnCount?: number;
+  fullWidth?: boolean;
+  fullHeight?: boolean;
+};
+
+export function Grid(props: GridProps) {
+  const { gap, rowGap, columnGap, columnCount, fullWidth, fullHeight, className, children } = props;
+
+  return (
+    <Box
+      className={cn(grid, className)}
+      style={{
+        gap: gap,
+        rowGap: rowGap,
+        columnGap: columnGap,
+        gridTemplateColumns: `repeat(${columnCount}, 1fr)`,
+        width: fullWidth ? '100%' : undefined,
+        height: fullHeight ? '100%' : undefined,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
