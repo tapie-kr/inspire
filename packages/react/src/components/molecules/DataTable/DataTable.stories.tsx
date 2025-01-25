8fc7e6bd5944f2d66a944412e72463075f874e06
cottons-kr <taeyeong.kim@algorix.io>
2025-01-25 17:58:24 +0900

    ✅ add FullExample story for DataTable component; integrate Filter and Pagination components for demonstration

--- packages/react/src/components/molecules/DataTable/DataTable.stories.tsx
+++ packages/react/src/components/molecules/DataTable/DataTable.stories.tsx
@@ -1,4 +1,11 @@
+import { spacingVars } from '@/lib/style/contract/component.css';
+
+import { GlyphIcon } from '@/components/foundations/Icon/icon-set';
+import { VStack } from '@/components/miscellaneous/layout/VStack';
+
 import { type Meta, type StoryObj } from '@storybook/react';
+import { Filter } from '../Filter';
+import { Pagination } from '../Pagination';
 import { DataTable } from '.';
 
 const meta: Meta = {
@@ -49,6 +56,104 @@ export const Default: DataTableStory = {
         },
       ]}
     />
+  ),
+};
+
+export const FullExample: DataTableStory = {
+  render: () => (
+    <VStack
+      fullWidth
+      spacing={spacingVars.medium}
+    >
+      <VStack
+        fullWidth
+        spacing={spacingVars.petite}
+      >
+        <Filter
+          filters={[
+            {
+              label: '상태',
+              options: [
+                {
+                  label: '전체',
+                  value: GlyphIcon.PALETTE,
+                },
+                {
+                  label: '활성',
+                  value: GlyphIcon.ADD,
+                },
+                {
+                  label: '비활성',
+                  value: GlyphIcon.CLOSE,
+                },
+              ],
+            },
+            {
+              label: '타입1234',
+              options: [
+                {
+                  label: '전체',
+                  value: GlyphIcon.PALETTE,
+                },
+                {
+                  label: '타입1',
+                  value: GlyphIcon.ADD,
+                },
+                {
+                  label: '타입2',
+                  value: GlyphIcon.CLOSE,
+                },
+              ],
+            },
+          ]}
+        />
+        <DataTable
+          columns={[
+            {
+              key: 'name',
+              label: 'Name',
+              width: 200,
+              isSortable: true,
+            },
+            {
+              key: 'age',
+              label: 'Age',
+              width: 100,
+              isSortable: true,
+              cell: age => `${age} years old`,
+            },
+            {
+              key: 'job',
+              label: 'Job',
+              width: 300,
+            },
+          ]}
+          data={[
+            {
+              name: 'John Doe',
+              age: 32,
+              job: 'Software Engineer',
+            },
+            {
+              name: 'Jane Doe',
+              age: 31,
+              job: 'Designer',
+            },
+            {
+              name: 'James Doe',
+              age: 30,
+              job: 'Product Manager',
+            },
+          ]}
+        />
+      </VStack>
+      <Pagination
+        min={1}
+        max={20}
+        visiblePages={10}
+        defaultPage={1}
+      />
+    </VStack>
   ),
 };
 
