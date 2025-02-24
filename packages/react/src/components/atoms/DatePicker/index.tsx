f133a9dee9ba53304aa51a69ae23f7e045cf3579
Taeyeong Kim <taeyeong.kim@algorix.io>
2025-02-25 00:09:43 +0900

    ✨ add DatePicker component with styles and Storybook integration; include 'use client' directive

---
+++ packages/react/src/components/atoms/DatePicker/index.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { base, input } from './styles.css';
+
+import { HStack } from '@/components/miscellaneous/layout/HStack';
+
+import { InputHTMLAttributes } from 'react';
+
+type DatePickerProps = InputHTMLAttributes<HTMLInputElement> & {
+  withTime?: boolean;
+};
+
+export function DatePicker(props: DatePickerProps) {
+  return (
+    <HStack
+      fullWidth
+      tag='label'
+      className={base}
+    >
+      <input
+        className={input}
+        type={props.withTime ? 'datetime-local' : 'date'}
+        {...props}
+      />
+    </HStack>
+  );
+}
